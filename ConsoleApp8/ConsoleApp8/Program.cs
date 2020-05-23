0af185a0c4b5c85de5b314c93e4a70a05cafa97c
polina-shimolina <shimolina.polina@gmail.com>
2020-05-23 16:30:42 +0300

    e

---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,30 @@
+﻿using ConsoleApp8;
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Examples.System.Net
+{
+    public class WebRequestGetExample
+    {
+        public static void Main()
+        {
+
+            WebRequest request = WebRequest.Create("https://api.openweathermap.org/data/2.5/weather?id=2172797&units=metric&appid=ecc0ea77210f8032ef8804a80a9f40f0");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Console.WriteLine(response.StatusDescription);
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+            Console.WriteLine(responseFromServer);
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+            Weather weather = JsonConvert.DeserializeObject<Weather>(responseFromServer);
+            Console.WriteLine($" t in {weather.name}: {weather.main.temp} C");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp8/ConsoleApp8/Weather.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Weather
+    {
+        public Tempreture main { get; set; }
+        public string name { get; set; }
+    }
+}
